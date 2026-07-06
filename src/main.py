250af9e19768e78963081aa001ac88373be6bc9d
Aryan Dubey <aryandubey@umass.edu>
2026-07-06 16:48:30 +0300

    added poisson calculations for attack strength for all teams

---
+++ src/main.py
@@ -0,0 +1,4 @@
+from setup1.setup import setup
+def main():
+    setup()
+
---
+++ src/poisson.py
@@ -0,0 +1,24 @@
+from setup1.db import get_connection
+import psycopg2
+
+def poisson_calc():
+    con = get_connection()
+    cursor = con.cursor()
+    overall_query = """SELECT SUM(goals_scored * tournament_weight) / SUM(tournament_weight)
+    FROM (
+            SELECT home_score as goals_scored, tournament_weight FROM matches
+            UNION ALL
+            SELECT away_score as goals_scored, tournament_weight FROM matches
+         ) as all_matches"""
+    cursor.execute(overall_query)
+    overall_avg_goals= cursor.fetchone()[0]
+
+    team_query = """SELECT team_id, SUM(goals_scored * tournament_weight) / SUM(tournament_weight) / %s 
+        FROM 
+        (SELECT home_team_id as team_id, home_score as goals_scored, tournament_weight FROM matches     
+         UNION ALL     
+         SELECT away_team_id as team_id, away_score as goals_scored, tournament_weight FROM matches 
+        ) as all_matches GROUP BY team_id;
+    """
+    cursor.execute(team_query,(overall_avg_goals,))
+    attack_strength = cursor.fetchall()
